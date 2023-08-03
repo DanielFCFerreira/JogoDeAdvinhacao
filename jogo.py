8041db9210fb841515cd4231c6c5e2c3e652add8
Daniel Ferreira <danielcoutinhoferreira@gmail.com>
2023-08-03 17:04:49 -0300

    Adicionando a funcionalidade de escolha de dificuldade e pontuação do jogador

--- jogo.py
+++ jogo.py
@@ -5,7 +5,23 @@ print("**Bem vindo ao jogo de Advinhação**")
 print("**********************************")
 
 numero_secreto = round(random.randrange(1, 101))
-tentativas = 5
+tentativas = 0
+pontos = 1000
+
+print("Qual o nível de dificuldade?")
+print("(1)Fácil (2)Médio (3)Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if nivel == 1:
+    print("Dificuldade Fácil")
+    tentativas = 20
+elif nivel == 2:
+    print("Dificuldade Média")
+    tentativas = 10
+else:
+    print("Dificuldade Difícil")
+    tentativas = 5
 
 for rodada in range(1, tentativas + 1):
 
@@ -25,6 +41,7 @@ for rodada in range(1, tentativas + 1):
     if acertou:
         print("**********************************")
         print("****Parabéns você acertou!!!!!!***")
+        print(f"****Você fez {pontos} pontos***********")
         print("**********************************")
         break
     else:
@@ -32,4 +49,5 @@ for rodada in range(1, tentativas + 1):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif menor:
             print("Você errou! O seu chute foi menor do que o número secreto.")
-
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
